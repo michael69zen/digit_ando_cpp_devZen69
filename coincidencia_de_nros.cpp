1ba6222fd9b9ebeb3be09a94faeff4cb64592eb6
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-04-28 17:34:18 -0500

    leer_3nros_y_un_4to_identificar_si_es_igual_a_alguno_anterior

---
+++ coincidencia_de_nros.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n1,n2,n3,n4;
+    cout<<"DIGITE N1: ";
+    cin>>n1;
+    cout<<"DIGITE N2: ";
+    cin>>n2;
+    cout<<"DIGITE N3: ";
+    cin>>n3;
+    cout<<"DIGITE N4: ";
+    cin>>n4;
+
+    if(n4==n1){
+        cout<<"N4 = N1";
+    }else if(n4==n2){
+        cout<<"N4 = N2";
+    }else if(n4==n3){
+        cout<<"N4 = N3";
+    }else{
+        cout<<"N4 es DIFERENTE";
+    }
+
+    return 0;
+}
+/*
+      By devZen69
+
+        $ $$$ $
+         $ $ $
+        $$$$$$$
+*/
