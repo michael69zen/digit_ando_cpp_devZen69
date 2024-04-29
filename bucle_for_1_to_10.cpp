d52af2782cd77ac93546d3256a6e8220ff3a70ff
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-04-29 07:00:40 -0500

    bucle_for_1_to_10.cpp

---
+++ bucle_for_1_to_10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    for(int i=1 ; i<=10 ; i+=1){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
+/*
+      By devZen69
+
+        $ $$$ $
+         $ $ $
+        $$$$$$$
+*/
