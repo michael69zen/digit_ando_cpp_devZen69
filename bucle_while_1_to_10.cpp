b63c925175a6b06779b0f48f626c9b5ffba3c95f
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-04-29 06:52:32 -0500

    bucle_while_imprimir_nros_1_to_10

---
+++ bucle_while_1_to_10.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i;
+    i=1;
+
+    while(i<=10){
+        cout<<i<<" ";
+        i+=1;
+    }
+
+    return 0;
+}
+/*
+      By devZen69
+
+        $ $$$ $
+         $ $ $
+        $$$$$$$
+*/
