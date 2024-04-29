7fc11da2d1efb385d173b7706f9e962a161d40aa
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-04-29 06:58:52 -0500

    bucle_do_while_1_to_10

---
+++ bucle_do_while_1_to_10.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i;
+    i=10;
+
+    while(i>=1){
+        cout<<i<<" ";
+        i-=1;
+    }
+
+    return 0;
+}
+/*
+      By devZen69
+
+        $ $$$ $
+         $ $ $
+        $$$$$$$
+*/
